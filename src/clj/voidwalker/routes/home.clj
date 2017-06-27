baa122218a68e0ce2b9e36afc54c0eb44451709c
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2017-06-27 20:24:04 +0530

    :sparkles: Add html5 routing

---
+++ src/clj/voidwalker/routes/home.clj
@@ -0,0 +1,18 @@
+(ns voidwalker.routes.home
+  (:require [voidwalker.layout :as layout]
+            [compojure.core :refer [defroutes GET]]
+            [ring.util.http-response :as response]
+            [clojure.java.io :as io]
+            [compojure.core :refer [ANY]]))
+
+(defn home-page []
+  (layout/render "home.html"))
+
+(defroutes home-routes
+  (GET "/" []
+       (home-page))
+  (GET "/docs" []
+       (-> (response/ok (-> "docs/docs.md" io/resource slurp))
+           (response/header "Content-Type" "text/plain; charset=utf-8")))
+  (ANY "*" []
+       (home-page)))
---
+++ src/cljs/voidwalker/db.cljs
@@ -0,0 +1,4 @@
+(ns voidwalker.db)
+
+(def default-db
+  {:page :home})
